0d6fbcad9328c152d08a9d0819d9a882ec5a4518
Chava <salvador.aguilar@amin-tech.com>
2025-10-28 17:06:21 -0600

    add: styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -175,8 +175,6 @@ export default function App() {
               />
               <div className="mt-3 flex items-center gap-3">
 
-
-
                 {/* Botones*/}
                 <label className="relative inline-flex items-center justify-center cursor-pointer">
                   <input type="file" accept="image/*" onChange={handleFileChange} className="sr-only" />
